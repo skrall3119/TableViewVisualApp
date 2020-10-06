8317ec044248abbc4e2569f20c16665a881e252a
skrall3119 <alexjanci@yahoo.com>
2020-10-06 02:17:43 -0500

    init

--- TableViewVisualApp/ViewController.swift
+++ TableViewVisualApp/ViewController.swift
@@ -7,7 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        petArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID")
+        cell?.textLabel?.text = petArray[indexPath.row]
+        return cell!
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let alert = UIAlertController(title: "Cell Content", message: "\(petArray[indexPath.row])", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: {action -> Void in})
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    let petArray = ["cat", "dog", "parakeet", "parrot", "canary", "finch", "tropical fish", "goldfish", "sea horses", "hamster", "gerbil", "rabbit", "turtle", "snake", "lizard", "hermit crab"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
